1e2b5ae71be23a48f30625f21fb7a36348f3d0d4
Josh Cummings <josh.cummings@gmail.com>
2018-12-28 14:59:07 -0700

    Use Branch Descriptions
    checkout-lesson now relies on a branch description and checks out
    the commit just before the first commit on the mitigation track.
    
    This way, branches aren't forced to do setup-like work on the first
    commit.

--- checkout-lesson.sh
+++ checkout-lesson.sh
@@ -3,5 +3,5 @@
 # Use this to check out the first track in a particular mitigation sequence
 # ./checkout-lesson.sh sql-injection
 
-git checkout `git rev-list master..$1 | tail -1`
-git log -n1
+git config branch.$1.description
+git checkout `git rev-list master..$1 | tail -1`~1 > /dev/null 2>&1

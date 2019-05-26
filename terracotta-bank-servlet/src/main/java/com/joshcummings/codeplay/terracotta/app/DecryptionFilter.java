a27453b52859d32ef25694869c1662a9493c621c
Josh Cummings <josh.cummings@gmail.com>
2019-05-25 20:32:40 -0600

    Step 3: Using GCM for Authenticated Encryption
    Stronger (and faster) than CBC is GCM. It's stronger because it gives
    us integrity of the IV and any other associated data. Happily, it's
    also faster since it can be parallelized.
    
    The specification indicates that the IV should be 12-bytes in length
    for ideal performance.
    
    AES GCM is the preferred algorithm for symmetric encryption.

--- terracotta-bank-servlet/src/main/java/com/joshcummings/codeplay/terracotta/app/DecryptionFilter.java
+++ terracotta-bank-servlet/src/main/java/com/joshcummings/codeplay/terracotta/app/DecryptionFilter.java
@@ -21,6 +21,7 @@ import org.springframework.util.StreamUtils;
 import javax.crypto.Cipher;
 import javax.crypto.CipherInputStream;
 import javax.crypto.SecretKey;
+import javax.crypto.spec.GCMParameterSpec;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import javax.servlet.Filter;
@@ -91,8 +92,9 @@ public class DecryptionFilter implements Filter {
 	private HttpServletRequest wrap(String version, HttpServletRequest request) throws Exception {
 		String ivHeader = request.getHeader("X-Encryption-Iv");
 		byte[] iv = Base64.getDecoder().decode(ivHeader);
-		Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-		cipher.init(Cipher.DECRYPT_MODE, secretKey(), new IvParameterSpec(iv));
+		Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
+		cipher.init(Cipher.DECRYPT_MODE, secretKey(), new GCMParameterSpec(128, iv));
+		cipher.updateAAD(version.getBytes(UTF_8));
 		InputStream plain = new CipherInputStream(new Base64InputStream(request.getInputStream()), cipher);
 		return new DecryptedWrapper(request, plain);
 	}
